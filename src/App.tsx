5543e9e3ce795900a3bb0227eecda8e339538af9
Julia-proj <juleea0@gmail.com>
2025-09-27 00:05:17 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -565,7 +565,7 @@ export default function App() {
 
           {/* 4 фото-отзыва из /public/images/reviews/… */}
           <div className="grid sm:grid-cols-2 md:grid-cols-4 gap-4 mt-12">
-            {[1, 2, 3, 4].map((n) => (
+            {[review1, review2, review3, reviews4].map((n) => (
               <img
                 key={n}
                 src={`/images/reviews/${n}.jpg`}
@@ -578,12 +578,12 @@ export default function App() {
           {/* 6 ссылок на видео из Instagram */}
           <div className="grid md:grid-cols-3 gap-4 mt-8">
             {[
-              "#insta-video-1",
-              "#insta-video-2",
-              "#insta-video-3",
-              "#insta-video-4",
-              "#insta-video-5",
-              "#insta-video-6",
+              "#insta-video-1",https://www.instagram.com/reel/DJjUiEnM-A_
+              "#insta-video-2",https://www.instagram.com/reel/DJmUkiNsZe1
+              "#insta-video-3",https://www.instagram.com/reel/DFX57cQobmS
+              "#insta-video-4",https://www.instagram.com/reel/DJoAXfKs6tu
+              "#insta-video-5",https://www.instagram.com/reel/DNG1lAPoCF7
+              "#insta-video-6",https://www.instagram.com/reel/DGmY70NIwz7/?igsh=NjB6bHN6ZWI0dHV6
             ].map((href, i) => (
               <a
                 key={i}
